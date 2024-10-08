1929bb8297f5516f085e3f0c2610cada5ea2c925
apil-dotcom <apilneupane19@gmail.com>
2024-10-08 15:33:44 +0545

    db connection done

--- projectsuru/settings.py
+++ projectsuru/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'auth',
-    'crud'
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +76,12 @@ WSGI_APPLICATION = "projectsuru.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "pythonsikdai",
+        "HOST":"localhost",
+        "USER":"root",
+        "PASSWORD":"",
+        "PORT":"3306"
     }
 }
 
