2b91c92b6c3ed4691f41e2ad5c2de10987336ee9
apil-dotcom <apilneupane19@gmail.com>
2024-10-14 11:21:21 +0545

    routin ko kaam sakiyo

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views.main_view import home
+from .views.main_view import create_blog,edit_blog,single_blog
+from .views.auth_view import register,login
+
+urlpatterns = [
+    path("",home),
+    path("register/",register),
+    path("login/",login),
+    path("create/",create_blog),
+    path("edit/",edit_blog),
+    path("single/",single_blog)
+]
