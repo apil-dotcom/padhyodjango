f3d8c42e25065037f5735cddb1c46599c9d9c170
apil-dotcom <apilneupane19@gmail.com>
2024-10-16 13:32:11 +0545

    models maa table ko kaam vayo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blogs(models.Model):
+    title=models.CharField(max_length=255,blank=False )
+    subtitle=models.CharField(max_length=255,blank=False,null=False )
+    description=models.TextField()
+    image=models.ImageField(upload_to='images',blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
