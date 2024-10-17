b7c6b098d8d0c0c71b17092b0d576304edac0352
apil-dotcom <apilneupane19@gmail.com>
2024-10-17 10:54:06 +0545

    post method bata data pathaune kaam vayo

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from .views.main_view import create_blog,edit_blog,single_blog
 from .views.auth_view import register,login
 
 urlpatterns = [
-    path("",home),
-    path("register/",register),
+    path("",home,name="home"),
+    path("register/",register,name="register"),
     path("login/",login),
     path("create/",create_blog),
     path("edit/",edit_blog),
