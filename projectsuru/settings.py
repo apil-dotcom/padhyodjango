2f73a4ad567482984d5f297f0905a602059f93a3
apil-dotcom <apilneupane19@gmail.com>
2024-10-21 17:22:46 +0545

    authentication and image ko kaam vayo

--- projectsuru/settings.py
+++ projectsuru/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+LOGIN_URL="/blog/logij"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
