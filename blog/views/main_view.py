594a8140b6cbdee807ecf5cabb97b1dc9868b28d
apil-dotcom <apilneupane19@gmail.com>
2024-10-22 12:15:25 +0545

    dynamic data show gariyo home page maa

--- blog/views/main_view.py
+++ blog/views/main_view.py
@@ -3,7 +3,11 @@ from ..models import Blogs
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request,'main/home.html')
+    blogs=Blogs.objects.all()
+    [
+
+    ]
+    return render(request,'main/home.html',{'blogs':blogs})
 
 def single_blog(request):
     return render(request,"main/single_blog.html")
--- projectsuru/settings.py
+++ projectsuru/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-LOGIN_URL="/blog/logij"
 
+MEDIA_URL="media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+
+LOGIN_URL="/blog/login"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
